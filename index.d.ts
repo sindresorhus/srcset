3a356616b50c8a1211fa095ca82e912acda54dd4
Sindre Sorhus <sindresorhus@gmail.com>
2021-05-12 00:17:05 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -7,10 +7,12 @@ declare namespace srcset {
 
 	interface SrcSetOptions {
 		/**
-		 * When strict mode is enabled, errors will be thrown on invalid input.
-		 * When disabled, a best effort will be made to handle invalid input and no errors will be thrown. The output may be invalid.
-		 * @default true
-		 */
+		When strict mode is enabled, errors will be thrown on invalid input.
+
+		When disabled, a best effort will be made to handle invalid input and no errors will be thrown. The output may be invalid.
+
+		@default true
+		*/
 		strict?: boolean;
 	}
 }
@@ -22,9 +24,6 @@ declare const srcset: {
 	Accepts a srcset string and returns an array of objects with the possible properties: `url` (always), `width`, `density`, and `height`.
 
 	@param srcset - A srcset string.
-
-	@param [options]
-	@param [options.strict=true] - Enable or disable validation of the srcset string. When enabled, an invalid srcset string will cause an error to be thrown. When disabled, a best effort will be made to parse the string, potentially resulting in invalid or nonsensical output.
 
 	@example
 	```
@@ -49,9 +48,6 @@ declare const srcset: {
 	Stringify `SrcSetDefinition`s. Accepts an array of `SrcSetDefinition` objects and returns a srcset string.
 
 	@param SrcSetDefinitions - An array of `SrcSetDefinition` objects. Each object should have a `url` field and may have either `width` or `density`. When `options.strict` is set to false, a `height` field is also accepted, and multiple descriptors (`width`, `height`, and`density`) are accepted.
-
-	@param [options]
-	@param [options.strict=true] - Enable or disable validation of the SrcSetDefinitions. When true, invalid input will cause an error to be thrown. When false, a best effort will be made to stringify invalid input, likely resulting in invalid srcset value.
 
 	@returns A srcset string.
 
